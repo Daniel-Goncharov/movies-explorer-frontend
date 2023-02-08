c7506f9225597c24df8f5bcdd7c832c8fabaeb8b
Daniel Goncharov <goncharov10d@gmail.com>
2023-02-08 22:08:51 +0500

    Добавляет компанент saved-movies

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,12 +4,14 @@ import Profile from '../User/Profile/Profile';
 import Main from '../Main/Main';
 import NotFound from '../NotFound/NotFound';
 import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
 
 function App() {
   return (
     <Routes>
       <Route path = '/' element = {<Main isLoggedIn={false}/>}/>
       <Route path = '/movies' element = {<Movies isLoggedIn={true}/>}/>
+      <Route path = '/saved-movies' element = {<SavedMovies isLoggedIn={true}/>}/>
       <Route path = '/profile' element = {<Profile/>}/>
       <Route path = '*' element = {<NotFound/>}/>
     </Routes>
---
+++ src/components/SavedMovies/SavedMovies.js
@@ -0,0 +1,18 @@
+import './SavedMovies.css'
+import Footer from "../Layout/Footer/Footer";
+import Header from "../Layout/Header/Header";
+import MoviesCardList from "../MoviesCardList/MoviesCardList";
+import SearchForm from "../SearchForm/SearchForm";
+
+export default function SavedMovies() {
+  return (
+    <>
+      <Header isLoggedIn={true}/>
+      <main className="saved-movies">
+        <SearchForm/>
+        <MoviesCardList isSavedMovies={true}/>
+      </main>
+      <Footer/>
+    </>
+  )
+}
